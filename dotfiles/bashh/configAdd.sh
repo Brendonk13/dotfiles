0e68cccc37e8183ba6fca5f8c364c64179867274
Brendonk13 <brendonk13.bk@gmail.com>
2020-05-07 15:53:36 -0600

    added some new files

--- dotfiles/bashh/configAdd.sh
+++ dotfiles/bashh/configAdd.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-config add ~/.bashrc
-config add ~/.bash_profile
-config add ~/.vimrc
 config add ~/.i3/config
 config add ~/dotfiles/
+config add ~/.bashrc
+config add ~/.bash_profile
+config add ~/.profile
+config add ~/.vimrc
+config add ~/.latexmkrc
 
 config add ~/.config/polybar
 config add ~/.config/fusuma
@@ -13,6 +15,7 @@ config add ~/.config/nvim
 config add ~/.config/.ripgreprc
 config add ~/.config/compton.conf
 config add ~/.config/mimeapps.list
+config add ~/.config/zathura/zathurarc
 
 config add ~/Scripts/battest_cronjob
 config add ~/Scripts/redshift_toggle.sh
