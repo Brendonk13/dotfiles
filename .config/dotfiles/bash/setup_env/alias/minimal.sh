58e7bdad3bb668819784af40c7deaa770c66fce8
Brendonk13 <brendonk13.bk@gmail.com>
2021-05-29 12:17:46 -0500

    Move find_logs to minimal just cuz I really like it and think it only uses bash builtins

--- .config/dotfiles/bash/setup_env/alias/minimal.sh
+++ .config/dotfiles/bash/setup_env/alias/minimal.sh
@@ -27,6 +27,25 @@ else
 fi
 
 
+find_logs(){
+    # Search lsof for a process's open files, return results containing string: 'log'
+    # returns some bad results but thats 1 out of 5 results for systemd, less for smaller programs!
+    [ $# -ne 1 ] && echo -e "USAGE: find_logs program_name\nError: Need program_name arg, and only this arg" && return 1
+    local program_name="$1"
+    [ hash rg > /dev/null 2&>1 ] && grep_prg='rg' || grep_prg='grep'
+    local log_files
+    log_files="$(pgrep "$program_name" | sudo xargs -n1 lsof -p 2> /dev/null | $grep_prg log | awk '{print $NF}' | $grep_prg log)"
+    if [ -n "$log_files" ]; then
+        echo "$log_files"
+        return 0
+    else
+        echo "No log files found for program: $program_name with pid: $(pgrep "$program_name")"
+        return 1
+    fi
+}
+alias flog='find_logs'
+
+
 # spelling mistakes
 alias cd..='cd ..'
 alias cd-='cd -'
